f358fd89693a2a05878a68b5ad13049641397f2f
colorplease <alistairvtran@gmail.com>
2024-02-08 09:52:06 -0800

    MORE error exception catching
    TO-DO
    * add month and day future protection
    * add multiline writes for files

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+from datetime import date
+
 current_input = ""
-importing_grades = "CONTINUE"
+importing_grades = "c"
 input_valid = False
 
 def write_input_to_file():
@@ -35,18 +37,60 @@ def getDayCount(date):
 
 current_input = input("Enter the CLASS NAME ")
 f = open(current_input + ".txt", "w")
-while importing_grades == "CONTINUE":
-    while input_valid == False:
-        points_gotten = input("Enter the amount of points YOU EARNED for this assignment ")
-        num_input_checker(points_gotten)
-        print_spacer()
-    input_valid = False
+#DATE CURRENT
+today = date.today().strftime('%m/%d/%Y').split("/")
+day_current = today[0][1]
+month_current = today[1][1]
+year_current = today[2]
+while importing_grades.lower() == "c":
+    #POINTS OUT OF
     while input_valid == False:
          points_out_of = input("Enter the amount of TOTAL POINTS that were avaliable for that assignment ")
          num_input_checker(points_out_of)
          print_spacer()
     input_valid = False
-    date_assignment = input("Enter the date the assignment was due (MONTH/DAY/YEAR) ")
-    current_input = current_input + ";" + points_gotten + ";" + points_out_of + ";" + date_assignment
-    importing_grades = input("Type COMPLETE to plot your grade trend. Type CONTINUE to record another assignment ")
+    #POINTS EARNED
+    while input_valid == False:
+        points_gotten = input("Enter the amount of points YOU EARNED for this assignment ")
+        if int(points_out_of) - int(points_gotten) > 0:
+            num_input_checker(points_gotten)
+        print_spacer()
+    input_valid = False
+    #YEAR ASSIGNMENT
+    year = ""
+    while input_valid == False:
+        year = input("Enter the YEAR the assignment was due WITH 4 DIGITS (####) ")
+        if len(year) == 4:
+            if int(year_current) - int(year) >= 0:
+                num_input_checker(year)
+        print_spacer()
+    input_valid = False
+    #MONTH ASSIGNMENT
+    month = ""
+    while input_valid == False:
+        month = input("Enter the MONTH the assignment was due WITH NO LEADING ZEROES (#) or (##) ")
+        if month[0] != "0":
+            if int(month) < 13:
+                num_input_checker(month)
+        print_spacer()
+    input_valid = False
+    #DAY ASSIGNMENT
+    day = ""
+    while input_valid == False:
+        day = input("Enter the DAY the assignment was due WITH NO LEADING ZEROES (#) or (##) ")
+        if day[0] != "0":
+            num_input_checker(day)
+        print_spacer()
+    input_valid = False
+    #CONTINUE OR PLOT
+    while input_valid == False:
+        importing_grades = input("Type F to plot your grade trend. Type C to record another assignment ")
+        print_spacer()
+        if importing_grades.lower() != "f" and importing_grades.lower() != "c":
+            input_valid = False
+        else:
+            input_valid = True
+    input_valid = False
+    f.write(points_gotten + ";" + points_out_of + ";" + month + ";" + day + ";" + year + '\n')
+    f.close()
 

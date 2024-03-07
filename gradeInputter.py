66f31396ca780bbe8a508f4181caa95c30622c6d
colorplease <alistairvtran@gmail.com>
2024-03-07 09:55:43 -0800

    get tkinter working later

--- gradeInputter.py
+++ gradeInputter.py
@@ -1,69 +1,31 @@
-# Python Program to make a scrollable frame
-# using Tkinter
-  
-from tkinter import *
+import tkinter as tk
 
-current_input = ""
-  
-class ScrollBar:
-     
-    # constructor
-    def __init__(self):
-         
-        # create root window
-        root = Tk()
-  
-        # create a horizontal scrollbar by
-        # setting orient to horizontal
-        h = Scrollbar(root, orient = 'horizontal')
-  
-        # attach Scrollbar to root window at 
-        # the bootom
-        h.pack(side = BOTTOM, fill = X)
-  
-        # create a vertical scrollbar-no need
-        # to write orient as it is by
-        # default vertical
-        v = Scrollbar(root)
-  
-        # attach Scrollbar to root window on 
-        # the side
-        v.pack(side = RIGHT, fill = Y)
-          
-  
-        # create a Text widget with 15 chars
-        # width and 15 lines height
-        # here xscrollcomannd is used to attach Text
-        # widget to the horizontal scrollbar
-        # here yscrollcomannd is used to attach Text
-        # widget to the vertical scrollbar
-        t = Text(root, width = 30, height = 30, wrap = NONE,
-                 xscrollcommand = h.set, 
-                 yscrollcommand = v.set)
-  
-        # insert some text into the text widget
-        t.insert(END,current_input)
-  
-        # attach Text widget to root window at top
-        t.pack(side=TOP, fill=X)
-  
-        # here command represents the method to
-        # be executed xview is executed on
-        # object 't' Here t may represent any
-        # widget
-        h.config(command=t.xview)
-  
-        # here command represents the method to
-        # be executed yview is executed on
-        # object 't' Here t may represent any
-        # widget
-        v.config(command=t.yview)
-  
-        # the root window handles the mouse
-        # click event
-        root.mainloop()
- 
-# create an object to Scrollbar class
-current_input = input("Enter the CLASS NAME ")        
+class Lotfi(tk.Entry):
+    def __init__(self, master=None, **kwargs):
+        self.var = tk.StringVar()
+        tk.Entry.__init__(self, master, textvariable=self.var, **kwargs)
+        self.old_value = ''
+        self.var.trace('w', self.check)
+        self.get, self.set = self.var.get, self.var.set
 
-s = ScrollBar()
+    def check(self, *args):
+        if self.get().isdigit(): 
+            # the current value is only digits; allow this
+            self.old_value = self.get()
+        else:
+            # there's non-digit characters in the input; reject this 
+            self.set(self.old_value)
+
+#demo:
+window = tk.Tk()
+window.wm_geometry("800x600")
+window.resizable(False, False)
+window.title('Login')
+frame_NUMBER_OF_POINTS = tk.Frame(window)
+From_entry=Lotfi(window)
+From_entry.grid(column=0,row=0)
+From_entry.pack()
+MyLabel = tk.Label(window,text="test",font=('font name',25))
+MyLabel.pack()
+From_entry.place(relx=0.45, rely= 0.45, width=200, height=50)
+window.mainloop()

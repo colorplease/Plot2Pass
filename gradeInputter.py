34e8a7f8fe7197c6f7188d5ea932e532d7caa26a
colorplease <alistairvtran@gmail.com>
2024-03-14 09:14:59 -0700

    tkinter is working

--- gradeInputter.py
+++ gradeInputter.py
@@ -1,31 +1,82 @@
 import tkinter as tk
+from datetime import date
 
-class Lotfi(tk.Entry):
-    def __init__(self, master=None, **kwargs):
-        self.var = tk.StringVar()
-        tk.Entry.__init__(self, master, textvariable=self.var, **kwargs)
-        self.old_value = ''
-        self.var.trace('w', self.check)
-        self.get, self.set = self.var.get, self.var.set
+input_valid = False
 
-    def check(self, *args):
-        if self.get().isdigit(): 
-            # the current value is only digits; allow this
-            self.old_value = self.get()
-        else:
-            # there's non-digit characters in the input; reject this 
-            self.set(self.old_value)
+def num_input_checker(input_text):
+    global input_valid
+    if input_text.isnumeric() == False:
+        input_valid = False
+    elif input_text.isnumeric() == True:
+        input_valid = True
 
+today = date.today().strftime('%m/%d/%Y').split("/")
+day_current = today[0][1]
+month_current = today[1][1]
+year_current = today[2]
+
+def check_out_of(points_out_of):
+    #POINTS OUT OF
+    num_input_checker(points_out_of)
+    if input_valid == False:
+         Error_lbl.config(text="ERROR: TYPE VALID NUMBER")
+         From_entry.delete(0, "end")
+    else:
+        Error_lbl.config(text="")
+        Score_lbl.config(text=str(points_out_of))
+        From_entry.delete(0, "end")
+        #sets up window to input POINTS GOTTEN
+        windowInputGrade.title('how many points did you EARN for this assignment?')
+        Title_lbl.config(text="how many points did you EARN for this assignment?")
+        step_num = 1
+        input_valid = False
+
+def check_got(points_gotten):
+    if int(TOTAL_POINTS) - int(points_gotten) > 0:
+        num_input_checker(points_gotten)
+    else:
+        Error_lbl.config(text="ERROR: POINTS GOTTEN IS HIGHER THAN POINTS AVALIABLE")
+        From_entry.delete(0, "end")
+    if input_valid == False:
+        Error_lbl.config(text="ERROR: TYPE VALID NUMBER")
+        From_entry.delete(0, "end")
+    else:
+        Error_lbl.config(text="")
+        Score_lbl.config(text=str(points_gotten + " / " + TOTAL_POINTS))
+        From_entry.delete(0, "end")
+        input_valid = False
+
+#all window stuff
+TOTAL_POINTS = 0
+GOT_POINTS = 0
+step_num = 0
+def inputGrades():
+    if(step_num == 0):
+        TOTAL_POINTS = From_entry.get()
+        check_out_of(TOTAL_POINTS)
+    elif(step_num == 1):
+        GOT_POINTS = From_entry.get()
+        check_got(GOT_POINTS)
 #demo:
-window = tk.Tk()
-window.wm_geometry("800x600")
-window.resizable(False, False)
-window.title('Login')
-frame_NUMBER_OF_POINTS = tk.Frame(window)
-From_entry=Lotfi(window)
-From_entry.grid(column=0,row=0)
+#create window and set window size
+#sets up window to input TOTAL AVALIABLE
+windowInputGrade = tk.Tk()
+frame_InputGrades = tk.Frame(windowInputGrade)
+frame_InputGrades.grid(column=0, row=0, sticky="N", pady=100)
+frame_InputGrades.pack(fill="both", expand=True)
+windowInputGrade.wm_geometry("800x600")
+windowInputGrade.resizable(False, False)
+windowInputGrade.title('how many points were AVALIABLE for this assignment?')
+#textbox
+From_entry = tk.Entry(frame_InputGrades,font=('font name',45), justify="center")
+#label
+Title_lbl = tk.Label(frame_InputGrades,text="how many points were AVALIABLE for this assignment?",font=('font name',19))
+Title_lbl.pack()
 From_entry.pack()
-MyLabel = tk.Label(window,text="test",font=('font name',25))
-MyLabel.pack()
-From_entry.place(relx=0.45, rely= 0.45, width=200, height=50)
-window.mainloop()
+Submit_btn = tk.Button(frame_InputGrades, text="CONFIRM", command=inputGrades, font=('font name',19))
+Submit_btn.pack()
+Score_lbl = tk.Label(frame_InputGrades,text="",font=('font name',45))
+Score_lbl.pack()
+Error_lbl = tk.Label(frame_InputGrades,text="",font=('font name',30), fg='red')
+Error_lbl.pack()
+windowInputGrade.mainloop()
